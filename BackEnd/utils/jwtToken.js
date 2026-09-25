652f5367a076b031acd9e8e2941776a14a682382
sajina <159539890+SAJINASAJINA@users.noreply.github.com>
2026-09-25 16:01:28 +0530

    Fix cross-site authentication cookies

--- BackEnd/utils/jwtToken.js
+++ BackEnd/utils/jwtToken.js
@@ -1,12 +1,20 @@
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonwebToken();
-    res.status(statusCode).cookie("token", token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
-    }).json({
-        success: true,
-        message,
-        user,
-        token,
+  const token = user.generateJsonwebToken();
+
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+      ),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
     });
 };
